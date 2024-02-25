58ecdad9fdc06ea182b4a2120c3604199f44cef7
ArpedoX <Ufuktlh06@proton.me>
2024-02-25 12:15:49 +0300

    25.02.2024

--- src/script.js
+++ src/script.js
@@ -4,3 +4,12 @@ clock.innerHTML = new Date().toLocaleTimeString();
 setInterval(() => {
     clock.innerHTML = new Date().toLocaleTimeString();
 }, 1000)
+
+let sozlerp = document.getElementById("sozler");
+let sozlerl = [
+    "Ölmek, yaşama alışmak demektir.",
+    "Ve en nihayetinde yine kendine dönersin…",
+    "Sınırların farkına varmak, onları zaten aşmış olmak demektir.",
+]
+
+sozlerp.innerHTML = sozlerl[Math.floor(Math.random() * sozlerl.length)];
